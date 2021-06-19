a245074e1910a239750a95dc9b1cda758e5c2e10
tom-turner <72453954+tom-turner@users.noreply.github.com>
2021-06-19 14:22:27 +0100

    first push

--- index.js
+++ index.js
@@ -1,15 +1,49 @@
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
+const port = process.env.port || 5000;
+const bp = require('body-parser');
+var ffmpeg = require('fluent-ffmpeg');
+var command = ffmpeg();
 
-const PORT = process.env.PORT || 5000
+app.use(bp.json())
+app.use(bp.urlencoded({ extended: true }))
 
-
-
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
   res.render('index.ejs');
 });
+
+
+app.post('/ffmpeg', function (req, res) {
+
+ffmpeg()
+  .input(req.body.file)
+  .videoCodec('libx264')
+  .videoBitrate('4500k')
+  .size('1920x1080')
+  .format('flv')
+  .save(req.body.rtmp+'/'+req.body.key)
+
+  .on('stderr', function(stderrLine) {
+    console.log('Stderr output: ' + stderrLine);
+  })
+
+  .on('start', function(commandLine) {
+    console.log('Spawned Ffmpeg with command: ' + commandLine)
+  })
+
+  .on('progress', function(progress) {
+    console.log('Processing: ' + progress.percent + '% done')
+  })
+
+  .on('error', function(err, stdout, stderr) {
+    console.log('Cannot process video: ' + err.message);
+      return res.json({ error : 'Cannot process video: ' + err.message})
+  })
+
+})
 
 //
 // Starting the App
