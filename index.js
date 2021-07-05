fe3821cbbc2132f7c4df7f85071c4d06e3d74c97
Jasper Lyons <jasper.lyons@gmail.com>
2021-07-05 20:11:31 +0100

    parse urls and get the input url required to stream them

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = process.env.port || 5000;
 const bp = require('body-parser');
 var ffmpeg = require('fluent-ffmpeg');
 var command = ffmpeg();
+var getStreamableUrl = require('./scripts/get-streamable-url')
 
 app.use(bp.json())
 app.use(bp.urlencoded({ extended: true }))
@@ -24,45 +25,45 @@ function formatResponse(started, message) {
   }) + "\n"
 }
 
-app.post('/ffmpeg', function (req, res) {
+app.post('/ffmpeg', async function (req, res) {
+  if(!req.body.file || !req.body.rtmp || !req.body.key) {
+    return res.write(formatResponse(false, "Invalid Input"))
+  }
 
-if(!req.body.file || !req.body.rtmp || !req.body.key) {
-  return res.write(formatResponse(false, "Invalid Input"))
-}
+  res.write(formatResponse(false, "recieved"))
 
-res.write(formatResponse(false, "recieved"))
-
-ffmpeg()
-  .input(req.body.file)
-  .inputOption('-re')
-  .videoBitrate('4500k')
-  .format('flv')
-  .save(req.body.rtmp+'/'+req.body.key)
+  var stream = ffmpeg()
+  var inputs = await getStreamableUrl(req.body.file)
+  inputs.forEach(input => stream.input(input))
+  stream.inputOption('-re')
+    .videoBitrate('4500k')
+    .format('flv')
+    .save(req.body.rtmp+'/'+req.body.key)
 
 
-  .on('stderr', function(stderrLine) {
-    console.log('Stderr output: ' + stderrLine);
-    res.write(formatResponse(false, 'Error: ' + stderrLine))
-  }) 
+    .on('stderr', function(stderrLine) {
+      console.log('Stderr output: ' + stderrLine);
+      res.write(formatResponse(false, 'Error: ' + stderrLine))
+    }) 
 
-  .on('start', function(commandLine) {
-    console.log('Spawned Ffmpeg with command: ' + commandLine)
-    
-   // sends logs to socket.io - ffmpegLogs = 'Spawned Ffmpeg with command: ' + commandLine;
+    .on('start', function(commandLine) {
+      console.log('Spawned Ffmpeg with command: ' + commandLine)
 
-    res.write(formatResponse(true, 'Spawned Ffmpeg with command: ' + commandLine))
-  })
+      // sends logs to socket.io - ffmpegLogs = 'Spawned Ffmpeg with command: ' + commandLine;
 
-  .on('progress', function(progress) {
-    console.log('Processing: ' + progress.percent + '% done')
-    res.write(formatResponse(true, 'Processed: ' + progress.percent + '%'))
-  })
+      res.write(formatResponse(true, 'Spawned Ffmpeg with command: ' + commandLine))
+    })
 
-  .on('error', function(err, stdout, stderr) {
-    console.log('Cannot process video: ' + err.message);
-    res.write(formatResponse(false, 'Cannot process video: ' + err.message))
-  })
-  
+    .on('progress', function(progress) {
+      console.log('Processing: ' + progress.percent + '% done')
+      res.write(formatResponse(true, 'Processed: ' + progress.percent + '%'))
+    })
+
+    .on('error', function(err, stdout, stderr) {
+      console.log('Cannot process video: ' + err.message);
+      res.write(formatResponse(false, 'Cannot process video: ' + err.message))
+    })
+
 })
 
 app.post('/stop', function (req, res) {
