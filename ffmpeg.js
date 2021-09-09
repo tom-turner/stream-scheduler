771c0ef5a242777057c3057d0726b1252dd761c9
tom-turner <72453954+tom-turner@users.noreply.github.com>
2021-09-09 18:36:10 +0100

    Update ffmpeg.js

--- ffmpeg.js
+++ ffmpeg.js
@@ -25,7 +25,8 @@ async function start(body) {
 	var inputs = await getStreamableUrl(body.file)
 	inputs.forEach(input => stream.input(input))
 	stream.inputOption('-re')
-	.fps(29.97)
+	.fps(30)
+	.videoBitrate('9600k')
 	.format('flv')
 	.save(body.rtmp+'/'+body.key)
 
